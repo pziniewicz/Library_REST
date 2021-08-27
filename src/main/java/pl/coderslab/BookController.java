4fd575815f3c82789434a27b080fe0f6cd2eb19e
Pawel Ziniewicz <pziniewicz@Pawels-MacBook-Air.local>
2021-08-27 17:07:29 +0200

    update

--- src/main/java/pl/coderslab/BookController.java
+++ src/main/java/pl/coderslab/BookController.java
@@ -1,15 +1,42 @@
 package pl.coderslab;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/books")
 public class BookController {
+    private MockBookService mockBookService;
 
-    @RequestMapping("/helloBook")
-    public Book helloBook() {
-        return new Book(1L, "9788324631766", "Thinking in Java",
-                "Bruce Eckel", "Helion", "programming");
+    public BookController(MockBookService mockBookService) {
+        this.mockBookService = mockBookService;
     }
+
+    @GetMapping("")
+    @ResponseBody
+    public List<Book> getList() {
+        return mockBookService.getList();
+    }
+
+    @PostMapping("")
+    public void addBook(@RequestBody Book book) {
+        mockBookService.addNewBook(book);
+    }
+
+    @GetMapping("/{id}")
+    public Book getBook(@PathVariable Long id) {
+        return mockBookService.getBook(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteBook(@PathVariable Long id) {
+        mockBookService.deleteBook(id);
+    }
+
+    @PutMapping("/{id}")
+    public void editBook(@PathVariable Long id, @RequestBody Book book) {
+        mockBookService.editBook(id, book);
+    }
+
 }
--- src/main/java/pl/coderslab/MockBookService.java
+++ src/main/java/pl/coderslab/MockBookService.java
@@ -1,8 +1,50 @@
 package pl.coderslab;
 
-import java.awt.print.Book;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
 import java.util.List;
 
+@Component
 public class MockBookService {
-    private List<Book> list;
+    private static List<Book> list;
+    private static Long nextId = 5L;
+
+    public MockBookService() {
+        list = new ArrayList<>();
+        list.add(new Book(1L, "9788324631766", "Thiniking	in	Java", "Bruce	Eckel", "Helion", "programming"));
+        list.add(new Book(2L, "9788324627738", "Rusz	glowa	Java.", "Sierra	Kathy,	Bates	Bert", "Helion",
+                "programming"));
+        list.add(new Book(3L, "9780130819338", "Java	2.	Podstawy", "Cay	Horstmann,	Gary	Cornell", "Helion",
+                "programming"));
+        list.add(new Book(4L, "9780596007126", "Head First Design Patterns", "Eric Freeman", "O'Reilly",
+                "programming"));
+    }
+
+    public Book getBook(Long id) {
+        return list.stream().filter(b -> b.getId().equals(id)).findAny().get();
+    }
+
+    public static void addNewBook(Book book) {
+        book.setId(nextId);
+        list.add(book);
+        nextId++;
+    }
+
+    public static List<Book> getList() {
+        return list;
+    }
+
+    public void deleteBook(Long id) {
+        if (this.getBook(id) != null) {
+            list.remove(this.getBook(id));
+        }
+    }
+
+    public void editBook(Long id, Book book) {
+        if (this.getBook(id) != null) {
+            int i = list.indexOf(this.getBook(id));
+            list.set(i, book);
+        }
+    }
 }
