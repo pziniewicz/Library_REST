128087be39069578c0227f606f998262eace574a
Pawel Ziniewicz <pziniewicz@Pawels-MacBook-Air.local>
2021-08-27 11:58:44 +0200

    update Book and BookController

--- src/main/java/pl/coderslab/BookController.java
+++ src/main/java/pl/coderslab/BookController.java
@@ -1,4 +1,15 @@
 package pl.coderslab;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/books")
 public class BookController {
+
+    @RequestMapping("/helloBook")
+    public Book helloBook() {
+        return new Book(1L, "9788324631766", "Thinking in Java",
+                "Bruce Eckel", "Helion", "programming");
+    }
 }
